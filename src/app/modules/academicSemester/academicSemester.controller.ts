beed6b85b958662a475a6d83dd8ca6d7ec890f10
MAHossain1 <mahpro110@gmail.com>
2024-05-15 19:54:09 +0600

    Implemented pagination, filter to get all semesters

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,8 +1,11 @@
 import { AcademicSemester } from '@prisma/client';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { academicSemesterFilterableFields } from './academicSemester.constant';
 import { AcademicSemesterService } from './academicSemester.service';
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
@@ -16,6 +19,24 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, academicSemesterFilterableFields);
+  const options = pick(req.query, paginationFields);
+  console.log('filters:', filters);
+  console.log('options:', options);
+
+  const result = await AcademicSemesterService.getAllFromDB(filters, options);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Data retrieved successfully.',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const AcademicSemesterController = {
   insertIntoDB,
+  getAllFromDB,
 };
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,4 +1,10 @@
-import { AcademicSemester, PrismaClient } from '@prisma/client';
+import { IAcademicSemesterFilterableFields } from './academicSemester.interface';
+
+import { AcademicSemester, Prisma, PrismaClient } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import { academicSemesterSearchableFields } from './academicSemester.constant';
 
 const prisma = new PrismaClient();
 
@@ -12,6 +18,67 @@ const insertIntoDB = async (
   return result;
 };
 
+const getAllFromDB = async (
+  filters: IAcademicSemesterFilterableFields,
+  options: IPaginationOptions
+): Promise<IGenericResponse<AcademicSemester[]>> => {
+  const { searchTerm, ...filterData } = filters;
+
+  console.log(options);
+
+  const andConditions = [];
+  if (searchTerm) {
+    andConditions.push({
+      OR: academicSemesterSearchableFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map(key => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.AcademicSemesterWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+
+  const result = await prisma.academicSemester.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: 'desc',
+          },
+  });
+  const total = await prisma.academicSemester.count();
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const AcademicSemesterService = {
   insertIntoDB,
+  getAllFromDB,
 };
