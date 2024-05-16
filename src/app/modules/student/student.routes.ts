5bf7733e0dbfa250a4a07c5bb8a17f026cc2c811
MAHossain1 <mahpro110@gmail.com>
2024-05-16 08:45:33 +0600

    Create student service, controller, routes and implemented.

--- src/app/modules/student/student.routes.ts
+++ src/app/modules/student/student.routes.ts
@@ -1,7 +1,24 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { StudentController } from './student.controller';
+import { StudentValidation } from './student.validation';
 
 const router = express.Router();
 
-router.post('/');
+router.get('/', StudentController.getAllFromDB);
+router.get('/:id', StudentController.getByIdFromDB);
+
+router.post(
+  '/',
+  validateRequest(StudentValidation.create),
+  StudentController.insertIntoDB
+);
+router.patch(
+  '/:id',
+  validateRequest(StudentValidation.update),
+  StudentController.updateIntoDB
+);
+
+router.delete('/:id', StudentController.deleteFromDB);
 
 export const StudentRoutes = router;
