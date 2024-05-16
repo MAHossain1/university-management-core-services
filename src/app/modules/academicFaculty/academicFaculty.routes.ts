976b2857f517b418a7470ccf391e195ec7fddd59
MAHossain1 <mahpro110@gmail.com>
2024-05-16 08:03:30 +0600

    Created academic faculty service, controller, routes, etc.

---
+++ src/app/modules/academicFaculty/academicFaculty.routes.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicFacultyController } from './academicFaculty.controller';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
+
+const router = express.Router();
+
+router.get('/', AcademicFacultyController.getAllFromDB);
+router.get('/:id', AcademicFacultyController.getByIdFromDB);
+
+router.post(
+  '/',
+  validateRequest(AcademicFacultyValidation.create),
+  AcademicFacultyController.insertIntoDB
+);
+
+router.patch(
+  '/:id',
+  validateRequest(AcademicFacultyValidation.update),
+  AcademicFacultyController.updateOneInDB
+);
+
+router.delete('/:id', AcademicFacultyController.deleteByIdFromDB);
+
+export const AcademicFacultyRoutes = router;
