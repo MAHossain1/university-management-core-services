58a59e376c6c12590cfeaa32957dfc804de87f07
MAHossain1 <mahpro110@gmail.com>
2024-05-28 06:56:40 +0600

    Retrieved all offeredCourseClassSchedule using filter done.

--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.controller.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { offeredCourseClassScheduleFilterableFields } from './offeredCourseClassSchedule.constant';
 import { OfferedCourseClassScheduleService } from './offeredCourseClassSchedule.service';
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
@@ -15,6 +18,23 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, offeredCourseClassScheduleFilterableFields);
+  const options = pick(req.query, paginationFields);
+  const result = await OfferedCourseClassScheduleService.getAllFromDB(
+    filters,
+    options
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'OfferedCourse class schedule fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const OfferedCourseClassScheduleController = {
   insertIntoDB,
+  getAllFromDB,
 };
