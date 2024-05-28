ec873e6c4cf159d03153a5c7ba435ec54882d555
MAHossain1 <mahpro110@gmail.com>
2024-05-28 11:26:44 +0600

    Retrieved, updated, deleted single offeredCourseClassSchedule.

--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.routes.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.routes.ts
@@ -1,10 +1,29 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { OfferedCourseClassScheduleController } from './offeredCourseClassSchedule.controller';
 
 const router = express.Router();
 
 router.get('/', OfferedCourseClassScheduleController.getAllFromDB);
+router.get('/:id', OfferedCourseClassScheduleController.getAllFromDB);
 
-router.post('/', OfferedCourseClassScheduleController.insertIntoDB);
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  OfferedCourseClassScheduleController.insertIntoDB
+);
+
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  OfferedCourseClassScheduleController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  OfferedCourseClassScheduleController.updateOneInDB
+);
 
 export const OfferedCourseClassScheduleRoutes = router;
