fd90fb5eba94dc75c92cb971e12f2aeb630c9528
MAHossain1 <mahpro110@gmail.com>
2024-05-28 19:54:34 +0600

    Validated offeredCourseClassSchedule.

--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.routes.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.routes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { OfferedCourseClassScheduleController } from './offeredCourseClassSchedule.controller';
+import { OfferedCourseClassScheduleValidation } from './offeredCourseClassSchedule.validation';
 
 const router = express.Router();
 
@@ -10,12 +12,14 @@ router.get('/:id', OfferedCourseClassScheduleController.getAllFromDB);
 
 router.post(
   '/',
+  validateRequest(OfferedCourseClassScheduleValidation.update),
   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   OfferedCourseClassScheduleController.insertIntoDB
 );
 
 router.patch(
   '/:id',
+  validateRequest(OfferedCourseClassScheduleValidation.update),
   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   OfferedCourseClassScheduleController.updateOneInDB
 );
