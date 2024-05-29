6d5ba407ac2ddc352350d018d31f28b3f0747c27
MAHossain1 <mahpro110@gmail.com>
2024-05-29 08:19:21 +0600

    Created StudentSemesterRegistrationCourse model.

---
+++ prisma/migrations/20240529021853_student_semester_registration_course/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "student_semester_registration_courses" (
+    "semesterRegistraionId" TEXT NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "offeredCourseId" TEXT NOT NULL,
+    "offeredCourseSectionId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "student_semester_registration_courses_pkey" PRIMARY KEY ("semesterRegistraionId","studentId","offeredCourseId")
+);
+
+-- AddForeignKey
+ALTER TABLE "student_semester_registration_courses" ADD CONSTRAINT "student_semester_registration_courses_semesterRegistraionI_fkey" FOREIGN KEY ("semesterRegistraionId") REFERENCES "semester_registration"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "student_semester_registration_courses" ADD CONSTRAINT "student_semester_registration_courses_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "students"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "student_semester_registration_courses" ADD CONSTRAINT "student_semester_registration_courses_offeredCourseId_fkey" FOREIGN KEY ("offeredCourseId") REFERENCES "offered_course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "student_semester_registration_courses" ADD CONSTRAINT "student_semester_registration_courses_offeredCourseSection_fkey" FOREIGN KEY ("offeredCourseSectionId") REFERENCES "offered_course_section"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
