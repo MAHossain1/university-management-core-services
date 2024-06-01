130c2e059116ff6da38fd93c756439c7e0e7888e
MAHossain1 <mahpro110@gmail.com>
2024-06-01 18:47:37 +0600

    Create offeredCourseSectionClassSchedule while creating offeredCourseSection.

--- src/app/modules/offeredCourseSection/offeredCourseSection.interface.ts
+++ src/app/modules/offeredCourseSection/offeredCourseSection.interface.ts
@@ -1,4 +1,21 @@
+import { weekDays } from '@prisma/client';
+
 export type IOfferedCourseSectionFilterRequest = {
   searchTerm?: string | undefined;
   offeredCourseId?: string | undefined;
+};
+
+export type IClassSchedule = {
+  startTime: string;
+  endTime: string;
+  dayOfWeek: weekDays;
+  roomId: string;
+  facultyId: string;
+};
+
+export type IOfferedCourseSectionCreate = {
+  title: string;
+  maxCapacity: number;
+  offeredCourseId: string;
+  classSchedules: IClassSchedule[];
 };
